c1ce7cb0b6e6bdc0d1a1d1c2cd73ee5d10ab8a94
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-11-16 16:03:06 +0600

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = Integer.parseInt(input.nextLine());
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int row = 0; row < size; row++) {
+                for (int cell = 0; cell < size; cell++) {
+                    out.write(((row + 1) * (cell + 1) + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
