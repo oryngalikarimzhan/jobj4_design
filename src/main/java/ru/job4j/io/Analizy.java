41dadcac0558f954ff1029306f3f82c93c07dc94
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-11-17 23:13:35 +0600

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,20 +1,39 @@
 package ru.job4j.io;
 
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-
-
-    }
-
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
+        StringBuilder log = new StringBuilder();
+        try (BufferedReader in =
+                     new BufferedReader(
+                             new FileReader(source))) {
+            try (PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(target)))) {
+                String line;
+                while ((line = in.readLine()) != null) {
+                    String[] split = line.split(" ");
+                    if (log.length() == 0
+                            && (split[0].equals("500") || split[0].equals("400"))) {
+                        log = new StringBuilder(split[1]);
+                    } else if (log.length() != 0
+                            && (split[0].equals("200") || split[0].equals("300"))) {
+                        log.append(";").append(split[1]).append(";");
+                        out.println(log);
+                        log = new StringBuilder();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Analizy test = new Analizy();
+        test.unavailable("./data/server.log", "./data/unavailable.csv");
     }
 }
