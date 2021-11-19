3e5e53a7bfd6f663dee513f95b41bee2487668b5
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-11-19 22:16:03 +0600

    5. Валидация параметров запуска. [#246865 #230372]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,12 +9,24 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        String[] searchingArgs = validate(args);
+        Path start = Paths.get(searchingArgs[0]);
+        search(start, p -> p.toFile().getName().endsWith(searchingArgs[1]))
+                .forEach(System.out::println);
+    }
+
+    private static String[] validate(String[] args) {
         if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar target/search.jar . java");
         }
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1]))
-                .forEach(System.out::println);
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toFile().getAbsoluteFile()));
+        }
+        if (!start.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toFile().getAbsoluteFile()));
+        }
+        return args;
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
