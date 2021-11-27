73d61d7f6554911e807cf3ca4591f9220be84d3d
oryngalikarimzhan <oryngalikarimzhan@github.com>
2021-11-27 20:10:11 +0600

    0. JDBC [#6863 #233256]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -11,10 +11,10 @@ public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Config config = new Config("./data/app.properties");
         config.load();
-        Class.forName(config.value("d"));
-        String url = config.value("l");
-        String login = config.value("u");
-        String password = config.value("p");
+        Class.forName(config.value("driver"));
+        String url = config.value("url");
+        String login = config.value("login");
+        String password = config.value("password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
