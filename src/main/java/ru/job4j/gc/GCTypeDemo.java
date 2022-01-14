7e26ec4e92d13ab30c79c5c484f0c8d9dc03f626
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-01-15 01:32:08 +0600

    Refactoring

--- src/main/java/ru/job4j/gc/GCTypeDemo.java
+++ src/main/java/ru/job4j/gc/GCTypeDemo.java
@@ -8,7 +8,7 @@ public class GCTypeDemo {
         int length = 100;
         String[] data = new String[1_000_000];
         System.out.println("PID процесса: " + ProcessHandle.current().pid());
-        for (int i = 0; ; i = (i + 1) % data.length) {
+        for (int i = 0; i < 1000; i = (i + 1) % data.length) {
             data[i] = String.valueOf(
                     (char) random.nextInt(255)
             ).repeat(length);
