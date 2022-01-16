63d764fbd2b2b101a5e6e5782b82a127426bddcf
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-01-16 19:45:52 +0600

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class GeneratorTest {
+
+    /*@Test
+    public void generatorTest() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        String result = generator.produce(template, map);
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        Assert.assertThat(result, is(expected));
+    }
+
+    @Test(expected = Exception.class)
+    public void generatorTestTemplate() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        String result = generator.produce(template, map);
+    }
+
+    @Test(expected = Exception.class)
+    public void generatorTestValueMap() {
+        Generator generator = new SimpleGenerator();
+        String template = "I am a ${name}";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        String result = generator.produce(template, map);
+    }*/
+
+}
